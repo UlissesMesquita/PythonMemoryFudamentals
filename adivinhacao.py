c24e049ab734e43462ecb9bcd3bcb0505cc189df
Ulisses Mesquita <desenvolvimento@xpon.com.br>
2024-01-07 19:04:57 -0300

    Alternando IDE

---
+++ adivinhacao.py
@@ -0,0 +1,19 @@
+numero_secreto = 42
+
+chute_str = input("Digete seu número:")
+print("Você digitou: ", chute_str)
+chute = int(chute_str)
+
+acertou = chute == numero_secreto
+maior = chute > numero_secreto
+menor = chute < numero_secreto
+
+if(acertou):
+    print("Você Acertou!")
+else:
+    if(maior):
+        print("Você errou! Seu chute foi maior do que o numero secreto!", numero_secreto)
+    else:
+        print("Você errou! Seu chute foi menor do que o numero secreto! O numero secreto foi: ",  numero_secreto)
+
+print("Fim do Jogo!")
