16fb780df1d50115fa78176d695440bf17e9bfa1
Ulisses Mesquita <desenvolvimento@xpon.com.br>
2024-01-07 20:02:08 -0300

    Adicionando Laço While com numero de tentativas

--- adivinhacao.py
+++ adivinhacao.py
@@ -1,19 +1,28 @@
 numero_secreto = 42
+total_tentativas = 3
+rodada = 1
 
-chute_str = input("Digete seu número:")
-print("Você digitou: ", chute_str)
-chute = int(chute_str)
 
-acertou = chute == numero_secreto
-maior = chute > numero_secreto
-menor = chute < numero_secreto
+while(rodada <= total_tentativas):
 
-if(acertou):
-    print("Você Acertou!")
-else:
-    if(maior):
-        print("Você errou! Seu chute foi maior do que o numero secreto!", numero_secreto)
+    print("Tentativa: {} de {}".format(rodada, total_tentativas))
+
+    chute_str = input("Digete seu número:")
+    print("Você digitou: ", chute_str)
+    chute = int(chute_str)
+
+    acertou = chute == numero_secreto
+    maior = chute > numero_secreto
+    menor = chute < numero_secreto
+
+    if (acertou):
+        print("Você Acertou!")
     else:
-        print("Você errou! Seu chute foi menor do que o numero secreto! O numero secreto foi: ",  numero_secreto)
+        if (maior):
+            print("Você errou! Seu chute foi maior do que o numero secreto!", numero_secreto)
+        else:
+            print("Você errou! Seu chute foi menor do que o numero secreto! O numero secreto foi: ", numero_secreto)
 
-print("Fim do Jogo!")
+    print("Fim do Jogo!")
+
+    rodada = rodada + 1
